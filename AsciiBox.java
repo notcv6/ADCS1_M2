3fd1961b70b6eb075ded7a1c697425142f37e891
cv6 <30357859+notcv6@users.noreply.github.com>
2023-08-23 16:53:55 -0700

    fix to teacher's standards

--- AsciiBox.java
+++ AsciiBox.java
@@ -12,27 +12,27 @@ public class AsciiBox
     // Output header information.
     public static void main(String[]args) {
         System.out.println("\nName/Programmer: Andrew Hernandez" 
-        + "\nDate: 8/15/2023" 
-        + "\nAssignment: 2.1.1: AsciiBox test" 
-        + "\nPurpose: To generate an ASCII Box."
-        + "\n************************************************************************\n");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
+            + "\nDate: 8/15/2023" 
+            + "\nAssignment: 2.1.1: AsciiBox test" 
+            + "\nPurpose: To generate an ASCII Box."
+            + "\n************************************************************************\n");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
+        System.out.println("+++++++++++++++++++++++++");
     }
 }
