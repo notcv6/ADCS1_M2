57434751f0bd3725231d328a96262fc687d612d8
cv6 <30357859+notcv6@users.noreply.github.com>
2023-08-23 16:56:57 -0700

    another zzz fix

--- AsciiBox2.java
+++ AsciiBox2.java
@@ -12,27 +12,27 @@ public class AsciiBox2
     // Output header information.
     public static void main(String[]args) {
         System.out.println("\nName/Programmer: Andrew Hernandez" 
-        + "\nDate: 8/15/2023" 
-        + "\nAssignment: 2.1.2: AsciiBox2 test" 
-        + "\nPurpose: To generate an ASCII Box."
-        + "\n************************************************************************\n");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
-        System.out.println("waw                   waw");
-        System.out.println("awa                   awa");
-        System.out.println("waw                   waw");
-        System.out.println("awa                   awa");
-        System.out.println("waw                   waw");
-        System.out.println("awa                   awa");
-        System.out.println("waw                   waw");
-        System.out.println("awa                   awa");
-        System.out.println("waw                   waw");
-        System.out.println("awa                   awa");
-        System.out.println("waw                   waw");
-        System.out.println("awa                   awa");
-        System.out.println("waw                   waw");
-        System.out.println("awa                   awa");
-        System.out.println("wawawawawawawawawawawawaw");
-        System.out.println("awawawawawawawawawawawawa");
+            + "\nDate: 8/15/2023" 
+            + "\nAssignment: 2.1.2: AsciiBox2 test" 
+            + "\nPurpose: To generate an ASCII Box."
+            + "\n************************************************************************\n");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("+++++++++++++++++++++++++++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++    A+ Comp Sci    +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("+++                   +++");
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("+++++++++++++++++++++++++++");
     }
 }
