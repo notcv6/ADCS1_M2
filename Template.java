25e1e2ddf0f435db4308307fc1edfb2905df9338
cv6 <30357859+notcv6@users.noreply.github.com>
2023-08-23 16:55:18 -0700

    template format fix :D

--- Template.java
+++ Template.java
@@ -12,10 +12,10 @@ public class Template
     // Output header information.
     public static void main(String[]args) {
         System.out.println("\nName/Programmer: Andrew Hernandez" 
-        + "\nDate: 8/15/2023" 
-        + "\nAssignment: 2.1.x: Template" 
-        + "\nPurpose: To generate a template for future use."
-        + "\n*****************************************************************\n");
+            + "\nDate: 8/15/2023" 
+            + "\nAssignment: 2.1.x: Template" 
+            + "\nPurpose: To generate a template for future use."
+            + "\n*************************************************************\n");
 //345678901234567890123456789012345678901234567890123456789012345678901234567890123
     }
 }
