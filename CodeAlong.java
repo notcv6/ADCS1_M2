a593d66f82b4a3114c8f4fb1592f8f7d55408c6d
notcv6 <leethawkyt@gmail.com>
2023-08-25 09:34:43 -0700

    more changes hahahahaahkshlkfjsdlkjsdlksdjflksdjg

--- CodeAlong.java
+++ CodeAlong.java
@@ -2,7 +2,7 @@
  * 
  * Name/Programmer: Andrew Hernandez
  * Date: 8/25/2023
- * Assignment: 2.3.x: CodeAlong
+ * Assignment: 2.3.0: CodeAlong
  * Purpose: Code along to learn about input.
  * 
  */
@@ -15,10 +15,9 @@ public class CodeAlong
     public static void main(String[]args) {
         System.out.println("\nName/Programmer: Andrew Hernandez" 
             + "\nDate: 8/25/2023" 
-            + "\nAssignment: 2.3.x: CodeAlong" 
+            + "\nAssignment: 2.3.0: CodeAlong" 
             + "\nPurpose: Code along to learn about input."
             + "\n*************************************************************\n");
-//345678901234567890123456789012345678901234567890123456789012345678901234567890123
 
         // Variable declarations
         Scanner keyboard = new Scanner(System.in);
@@ -41,5 +40,24 @@ public class CodeAlong
         System.out.println("\nYour name is " + firstName + " " + lastName + 
             " and you are " + age + " years old!");
         
+        // Prompt another example
+        System.out.print("\n\nEnter your status at school: ");
+        status = keyboard.next();
+        System.out.print("Enter your age: ");
+        age = keyboard.nextInt();
+        System.out.print("Enter your full name: ");
+        keyboard.nextLine();
+        fullName = keyboard.nextLine();
+        System.out.print("Enter your school name: ");
+        schoolName = keyboard.next();
+        
+        // Output
+        System.out.println("\nYour full name is " + fullName + ", " +
+            "you are " + age + "\nyears old and you " + "are a " + 
+            status + " at " + schoolName + ".");
+            
+        // Ending statement
+        System.out.println("\n**********************************************");
+        System.out.println("Ending the code-along with Ms. Griswold.");
     }
 }
--- Template.java
+++ Template.java
@@ -11,12 +11,18 @@ public class Template
 {
     // Output header information.
     public static void main(String[]args) {
-        System.out.println("Name/Programmer: Andrew Hernandez" 
-            + "\nDate: x/x/2023" 
-            + "\nAssignment: 2.x.x: Template" 
-            + "\nPurpose: Placeholder."
-            + "\n*************************************************************\n");
-
+        // Opening statement.
+        System.out.println("Name/Programmer: Andrew Hernandez"
+            + "\nDate: "
+            + "\nAssignment: "
+            + "\nPurpose: "
+            + "\n****************************************************\n");
+        
+        // Begin program.
+        
             
+        // Ending statement.
+        System.out.println("****************************************************");
+        System.out.println("End program.");
     }
 }
