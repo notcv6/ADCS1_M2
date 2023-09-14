d30b10e90dbf69fe731dff42a3701e8d015743e9
fukatoyokawa <throwaway641334@gmail.com>
2023-09-14 09:53:33 -0700

    add new assignment

---
+++ MathClassMethods.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ * Name/Programmer: Andrew Hernandez
+ * Date: x/x/2023
+ * Assignment: 2.x.x: MathClassMethods
+ * Purpose: To generate a template for future use.
+ * 
+ */
+
+import java.util.Scanner;
+
+public class MathClassMethods
+{
+    // Output header information.
+    public static void main(String[]args) {
+        // Opening statement.
+        System.out.println("Name/Programmer: Andrew Hernandez"
+            + "\nDate: "
+            + "\nAssignment: "
+            + "\nPurpose: "
+            + "\n****************************************************\n");
+        
+        // Declare variables.
+        Scanner keyboard = new Scanner(System.in);
+        double inputValue;
+        
+        // Begin program.
+        // for loop here probably
+        System.out.print("Placeholder text");
+        inputValue = keyboard.nextDouble();
+        System.out.println("placeholder2: " + Math.sqrt(inputValue));
+        
+        // Ending statement.
+        System.out.println("\n*****************************************");
+        System.out.println("End program.");
+    }
+}
