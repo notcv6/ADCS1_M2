98e1bb4f1f87236538d3199120581df184ba42e8
cv6 <leethawkyt@gmail.com>
2024-04-25 09:15:52 -0700

    ermmmmm

--- Distances.java
+++ Distances.java
@@ -1,49 +1,103 @@
 /**
  * Name/Programmer: Andrew Hernandez
- * Date: April 15, 2024
- * Assignment: 6.1.2 Intro to 2D Arrays Pt. 2
- * Purpose: Introduction to 2D Arrays, Pt. 2
+ * Date: April 25, 2024
+ * Assignment: 6.1.3 2D Distances
+ * Purpose: Learn how to use 2D Arrays.
  */
 
  import java.util.Scanner;
- public class Distances
- {
-     public static void main(String[] args)
-     {
-        // Print out the header information.
-        System.out.println("\nName/Programmer: Andrew Hernandez");
-        System.out.println("Date: April 15, 2024");
-        System.out.println("Assignment: 6.1.2 Intro to 2D Arrays Pt. 2");
-        System.out.println("Purpose: Introduction to 2D Arrays, Pt. 2");
-        System.out.println("**************************************************");
-        System.out.println();
-         
-        // Declarations and initializations.
-        double[][] arr = {{-1, 3}, 
-                        {-1, -1}, 
-                        {1, 1}, 
-                        {2, 0.5}, 
-                        {2, -1}, 
-                        {3, 3}, 
-                        {4, 2}, 
-                        {4, -0.5}};
-
-        //
-        for (int row=0;row<arr.length;row++) {
-            int x = 0;
-        }
-
-        // Output the ending statement.
-        System.out.println();
-        System.out.println("**************************************************");
-        System.out.println("Ending Assignment <6.1.2>: Intro to 2D Arrays, Pt. 2");
+ public class Distances {
+     public static void main(String[] args) {
+         // Print out the header information.
+         System.out.println("\nName/Programmer: Andrew Hernandez");
+         System.out.println("Date: April 25, 2024");
+         System.out.println("Assignment: 6.1.3 2D Distances");
+         System.out.println("Purpose: Learn how to use 2D Arrays");
+         System.out.println("**************************************************");
+         System.out.println();
+ 
+         // Declarations and initializations.
+         double[][] arr = {{-1, 3}, {-1, -1}, 
+            {1, 1}, {2, 0.5}, 
+            {2, -1}, {3, 3}, 
+            {4, 2}, {4, -0.5}};
+ 
+         // Output the 2D array.
+         printIt(arr);
+         System.out.println();
+ 
+         // Determine the shortest distance between two points
+         double minDistance = 100;
+         double[] closestPoints = new double[4];
+ 
+         for (int i = 0; i < arr.length; i++) {
+             for (int j = i + 1; j < arr.length; j++) {
+                 double x1 = arr[i][0];
+                 double y1 = arr[i][1];
+                 double x2 = arr[j][0];
+                 double y2 = arr[j][1];
+ 
+                 double currentDistance = distance(x1, y1, x2, y2);
+ 
+                 if (currentDistance < minDistance) {
+                     minDistance = currentDistance;
+                     closestPoints[0] = x1;
+                     closestPoints[1] = y1;
+                     closestPoints[2] = x2;
+                     closestPoints[3] = y2;
+                 }
+             }
+         }
+ 
+         System.out.println("The closest points are (" + closestPoints[0] + ", " + closestPoints[1] + ") and (" + closestPoints[2] + ", " + closestPoints[3] + ")");
+         System.out.printf("The distance between them is %.2f", minDistance);
+         System.out.println();
+ 
+         // Determine the furthest distance between two points
+         double maxDistance = 0;
+         double[] farthestPoints = new double[4];
+ 
+         for (int i = 0; i < arr.length; i++) {
+             for (int j = i + 1; j < arr.length; j++) {
+                 double x1 = arr[i][0];
+                 double y1 = arr[i][1];
+                 double x2 = arr[j][0];
+                 double y2 = arr[j][1];
+                 double currentDistance = distance(x1, y1, x2, y2);
+                 if (currentDistance > maxDistance) {
+                     maxDistance = currentDistance;
+                     farthestPoints[0] = x1;
+                     farthestPoints[1] = y1;
+                     farthestPoints[2] = x2;
+                     farthestPoints[3] = y2;
+                 }
+             }
+         }
+ 
+         System.out.println();
+         System.out.println("The farthest points are (" + farthestPoints[0] + ", " + farthestPoints[1] + ") and (" + farthestPoints[2] + ", " + farthestPoints[3] + ")");
+         System.out.printf("The distance between them is %.2f", maxDistance);
+ 
+         // Output the ending statement.
+         System.out.println();
+         System.out.println("**************************************************");
+         System.out.println("Ending Assignment <6.1.3>: 2D Distances");
+     }
+     // printIt; Takes in a 2D array and prints it out.
+     // Accepts a 2D array as input.
+     public static void printIt(double[][] arr) {
+         System.out.println("The set of 2D points is: ");
+         for (int row = 0; row < arr.length; row++) {
+             System.out.println("\t(" + arr[row][0] + ", " + arr[row][1] + ")");
+         }
+     }
+ 
+     // distance; Determines the distances between two points.
+     // Accepts four doubles as input.
+     public static double distance(double x1, double y1, double x2, double y2) {
+         double x = x2 - x1;
+         double y = y2 - y1;
+         double distance = Math.sqrt(x * x + y * y);
+         return distance;
      }
  }
-
- public static void printIt(double[][] arr) {
-    for (int row=0; row<arr.length; row++) {
-        System.out.print("(" + arr[row][0] + ", " + arr[row][1] + ") ");
-    }
- }
-
- public static double distance(double)
--- Pennies.java
+++ Pennies.java
@@ -1,8 +1,8 @@
 /**
  * Name/Programmer: Andrew Hernandez
- * Date: April 15, 2024
- * Assignment: 6.1.2 Intro to 2D Arrays Pt. 2
- * Purpose: Introduction to 2D Arrays, Pt. 2
+ * Date: April 25, 2024
+ * Assignment: 6.1.4 Pitching Pennies
+ * Purpose: Learn how to use 2D arrays.
  */
 
  import java.util.Scanner;
@@ -12,9 +12,9 @@
      {
         // Print out the header information.
         System.out.println("\nName/Programmer: Andrew Hernandez");
-        System.out.println("Date: April 15, 2024");
-        System.out.println("Assignment: 6.1.2 Intro to 2D Arrays Pt. 2");
-        System.out.println("Purpose: Introduction to 2D Arrays, Pt. 2");
+        System.out.println("Date: April 25, 2024");
+        System.out.println("Assignment: 6.1.4 Pitching Pennies");
+        System.out.println("Purpose: Learn how to use 2D arrays.");
         System.out.println("**************************************************");
         System.out.println();
          
@@ -43,7 +43,9 @@
         do {
             System.out.print("Do you want to play the Penny Pitching Game? (Y/N): ");
             input = keyboard.nextLine();
+            System.out.println();
 
+            // Generate the locations of where the pennies will land.
             if (input.equals("Y")) {
                 int[][] coords = new int[5][2];
                 
@@ -53,41 +55,63 @@
                     }
                     System.out.println();
                 }
-
+                System.out.println();
+                
                 // Generate random numbers for x and y.
                 for (int i=0;i<5;i++) {
                     coords[i][0] = (int)(Math.random() * 5);
                     coords[i][1] = (int)(Math.random() * 5);
                     System.out.println("Pitch #"+i+" is at ("+coords[i][0]+", "+coords[i][1]+")");
                 }
-
+                System.out.println();
+                
+                // Calculate the total amount of points a player got.
+                int totalPoints = 0;
+                for (int i = 0; i < 5; i++) {
+                    int row = coords[i][0];
+                    int col = coords[i][1];
+                    if (arr[row][col] == 2) {
+                        totalPoints += 2;
+                    } else if (arr[row][col] == 3) {
+                        totalPoints += 3;
+                    } else {
+                        totalPoints += 1;
+                    }
+                }
+                
                 // Display the final board, replacing the numbers with P’s showing where the pennies landed and an x where no penny landed.
+                System.out.println("Your total is " + totalPoints + " and your final board is: ");
                 for (int row=0;row<arr.length;row++) {
                     for (int col=0;col<arr[row].length;col++) {
-                        boolean found = false;
+                        int count = 0;
                         for (int i=0;i<5;i++) {
                             if (coords[i][0] == row && coords[i][1] == col) {
-                                System.out.print("P ");
-                                found = true;
+                                count++;
                             }
                         }
-                        if (!found) {
-                            System.out.print("x ");
+                        if (count == 1) {
+                            System.out.print("P  ");
+                        } else if (count > 1) {
+                            System.out.print("PP ");
+                        } else {
+                            System.out.print("x  ");
                         }
                     }
                     System.out.println();
                 }
 
+                System.out.println();
             } else if (!input.equals("N") && !input.equals("Y")) {
                 System.out.println("Invalid input. Please try again.");
             } else {
                 break;
             }
         } while (input != "N");
+        System.out.println("Thank you for playing the Penny Pitching Game!");
 
         // Output the ending statement.
         System.out.println();
         System.out.println("**************************************************");
-        System.out.println("Ending Assignment <6.1.2>: Intro to 2D Arrays, Pt. 2");
+        System.out.println("Ending Assignment <6.1.4>: Pitching Pennies");
      }
  }
