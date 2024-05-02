e9a139e4502b1e944befa74cc6fb5b2c379af523
cv6 <leethawkyt@gmail.com>
2024-05-02 09:51:19 -0700

    wasd2

---
+++ Person2.java
@@ -0,0 +1,113 @@
+/**
+ * Name/Programmer: Andrew Hernandez
+ * Date: April 30, 2024
+ * Assignment: 7.1.1 Classes & Objects Introduction
+ * Purpose: Introduction to Classes & Objects
+ */
+
+public class Person2
+{
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String school;
+    private String program;
+    private int graduatingYear;
+
+    public Person2(String fn, String ln, int ag, String sc, String pg, int yr) {
+        firstName = fn; 
+        lastName = ln;
+        age = ag;
+        school = sc;
+        program = pg;
+        graduatingYear = yr;
+    }
+
+    public Person2() {
+        firstName = "John"; 
+        lastName = "Doe";
+        age = 999;
+        school = "unknown";
+        program = "unknown";
+        graduatingYear = 9999;
+    }
+    
+    // getFirstName; Returns the first name from a person object.
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // getLastName; Returns the last name from a person object.
+    public String getLastName() {
+        return lastName;
+    }
+
+    // getAge; Returns the age from a person object.
+    public int getAge() {
+        return age;
+    }
+
+    // getProgram; Returns the program from a person object.
+    public String getProgram() {
+        return program;
+    }
+
+    // getSchool; Returns the school from a person object.
+    public String getSchool() {
+        return school;
+    }
+
+    // getYear; Returns the graduating year from a person object.
+    public int getYear() {
+        return graduatingYear;
+    }
+
+    // setFirstName; Sets the first name of a person object.
+    // f; Accepts a string.
+    public void setFirstName(String f) {
+        firstName = f;
+    }
+
+    // setLastName; Sets the last name of a person object.
+    // l; Accepts a string.
+    public void setLastName(String l) {
+        lastName = l;
+    }
+
+    // setAge; Sets the age of a person object.
+    // a; Accepts an integer.
+    public void setAge(int a) {
+        age = a;
+    }
+
+    // setProgram; Sets the program of a person object.
+    // p; Accepts a string.
+    public void setProgram(String p) {
+        program = p;
+    }
+
+    // setSchool; Sets the school of a person object.
+    // s; Accepts a string.
+    public void setSchool(String s) {
+        school = s;
+    }
+
+    // setYear; Sets the graduating year of a person object.
+    // y; Accepts an integer.
+    public void setYear(int y) {
+        graduatingYear = y;
+    }
+
+    // toString; Returns a formatted string with the information of a person.
+    public String toString() {
+        if (age == 999 && graduatingYear == 9999 && program == "unknown" && school == "unknown") {
+            return firstName+" "+lastName+" is unknown years old, \n\tattends "
+        +school+"\n\tis in the "+program+" program, \n\tand will graduate in "
+        +"unknown.";
+        } else {
+            return firstName+" "+lastName+" is "+age+" years old, \n\tattends "
+        +school+"\n\tis in the "+program+" program, \n\tand will graduate in "
+        +graduatingYear+".";
+        }
+    }
+}
---
+++ PersonDriver2.java
@@ -0,0 +1,53 @@
+/**
+ * Name/Programmer: Andrew Hernandez
+ * Date: May 2, 2024
+ * Assignment: 7.1.2 Classes & Objects Introduction
+ * Purpose: Introduction to Classes & Objects
+ */
+
+public class PersonDriver2
+{
+    public static void main(String[] args)
+    {
+        // Output Header Information
+        System.out.println("\nName/Programmer: Andrew Hernandez");
+        System.out.println("Date: May 2, 2024");
+        System.out.println("Assignment: 7.1.2 Classes & Objects Introduction");
+        System.out.println("Purpose: Introduction to Classes & Objects");
+        System.out.println();
+        System.out.println("**************************************************");
+        
+        // Variable declarations
+        Person2 me = new Person2("Andrew", "Hernandez", 17
+            , "Southwest Career & Technical Academy", "Digital Game Design"
+            , 2025);
+        Person2 person = new Person2();
+
+        // Print out my information
+        System.out.println("First Name: "+me.getFirstName());
+        System.out.println("Last Name: "+me.getLastName());
+        System.out.println("Age: "+me.getAge());
+        System.out.println("Program: "+me.getProgram());
+        System.out.println("School: "+me.getSchool());
+        System.out.println("Graduating Year: "+me.getYear());
+
+        System.out.println();
+        System.out.println(me);
+        System.out.println();
+
+        System.out.println("First Name: "+person.getFirstName());
+        System.out.println("Last Name: "+person.getLastName());
+        System.out.println("Age: "+person.getAge());
+        System.out.println("Program: "+person.getProgram());
+        System.out.println("School: "+person.getSchool());
+        System.out.println("Graduating Year: "+person.getYear());
+
+        System.out.println();
+        System.out.println(person);
+        // Ending statement
+        System.out.println();
+        System.out.println("**************************************************");
+        System.out.println("Ending Assignment <7.1.2>: Classes & Objects");
+        System.out.println("\tIntroduction");
+    }
+}
